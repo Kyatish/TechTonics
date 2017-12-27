f1d4506451d602ba4ff1cf77d7f8669c9c7b61aa
Kyatish <yatish09kumar@gmail.com>
2017-12-27 22:33:02 +0530

    pom updated

--- TechTonics/src/utilities/DBUtilities.java
+++ TechTonics/src/utilities/DBUtilities.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 
 public class DBUtilities {
 	static String drvr = "com.mysql.jdbc.Driver";
-	static String url = "jdbc:mysql://localhost:3306/tech_tonics?useSSL=true";
+	static String url = "jdbc:mysql://10.128.158.1:3306/tech_tonics";
 	static String name = "root";
-	static String pass = "";
+	static String pass = "asdf123";
 
 	public static Connection getConnection() throws Exception {
 		Class.forName(drvr);
